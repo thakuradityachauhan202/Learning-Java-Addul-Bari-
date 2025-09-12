e06aaf0a0d8437bc24958ff90c083119a25d5e60
Aditya Singh <adityasingh@Adityas-MacBook-Air-2.local>
2025-09-13 03:14:50 +0530

    loop practice

--- src/loops.java
+++ src/loops.java
@@ -1,2 +1,14 @@
-public class loops {
+import java.lang.*;
+import java.util.*;
+public class loops
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        while(i<100)
+        {
+            System.out.println(i);
+            i=i*2;
+        }
+    }
 }
